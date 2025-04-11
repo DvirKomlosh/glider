173dc9d52e61ff389c248c3475ff98db0a43ce1d
Dvir Komlosh <dvirkomlosh@gmail.com>
2025-04-11 12:50:11 +0300

    screen shake on death and ring

---
+++ Scripts/camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+var shake_amount = 0.0
+var default_offset = Vector2(2000,2000)
+
+
+func _process(delta):
+	if shake_amount > 0:
+		offset = default_offset + Vector2(randf_range(-7, 7), randf_range(-7, 7)) * shake_amount
+		shake_amount = lerp(shake_amount, 0.0, delta * 10)
+	else:
+		offset = default_offset
+
+
+
+func start_screen_shake(amount: float):
+	shake_amount = amount
